78227c9e45e9e2207688bb3165ffe4bbbdb7cb81
GhostyOG-j <user@192.168.34.89>
2026-09-03 10:43:01 +0200

    Den Taschenrechner vervollständigt und  Die Test Klasse auch vervollständigt

--- Taschenrechner_v0/src/git_Taschenrechner/Taschenrechner.java
+++ Taschenrechner_v0/src/git_Taschenrechner/Taschenrechner.java
@@ -1,5 +1,25 @@
 package git_Taschenrechner;
 
 public class Taschenrechner {
+	
+	public double add(double zahl1, double zahl2) {
+		return zahl1 + zahl2;
+	}
+		
+		public double sub(double zahl1, double zahl2){
+			return zahl1 - zahl2;
+		}
+		
+		public double mul(double zahl1, double zahl2){
+			return zahl1 * zahl2;
+		}
 
-}
+		public double div(double zahl1, double zahl2){
+			return zahl1 / zahl2;
+		}
+	}
+		
+	
+	
+
+
--- Taschenrechner_v0/src/git_Taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner_v0/src/git_Taschenrechner/TaschenrechnerTest.java
@@ -1,5 +1,70 @@
 package git_Taschenrechner;
 
+import java.util.Scanner;
+
 public class TaschenrechnerTest {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner myScanner = new Scanner(System.in);
+		Taschenrechner ts = new Taschenrechner();
+
+		int swValue;
+
+		// Display menu graphics
+		System.out.println("============================");
+		System.out.println("|   MENU SELECTION DEMO    |");
+		System.out.println("============================");
+		System.out.println("| Options:                 |");
+		System.out.println("|        1. Addieren       |");
+		System.out.println("|        2. Subtrahieren   |");
+		System.out.println("|        3. Dividieren     |");
+		System.out.println("|        4. Multiplikation |");
+		System.out.println("|        5. Exit	   |");
+		System.out.println("============================");
+		System.out.print(" Select option: ");
+		swValue = myScanner.next().charAt(0);
+
+		if (swValue == '1' || swValue == '2' || swValue == '3' || swValue == '4') {
+			System.out.println("Geben sie die erste Zahl ein.");
+		double zahl1 = myScanner.nextDouble();
+		System.out.println("Geben sie die zweite Zahl ein.");
+		double zahl2 = myScanner.nextDouble();
+		
+		
+		
+		// Switch construct
+		switch (swValue) {
+		case '1':
+			
+			System.out.println( zahl1 + "+" + zahl2 + "=" + ts.add(zahl1, zahl2));
+			break;
+			
+		case '2':
+			
+			System.out.println( zahl1 + "-" + zahl2 + "=" + ts.sub(zahl1, zahl2));
+			break;
+		 
+			case '3':
+				
+				System.out.println( zahl1 + "/" + zahl2 + "=" + ts.div(zahl1, zahl2));
+				break;
+		  
+				case '4':
+				
+				System.out.println( zahl1 + "*" + zahl2 + "=" + ts.mul(zahl1, zahl2));
+				break;
+				case '5':
+					System.out.println("System Beenden");
+					break;
+		  
+		default:
+			System.out.println("Invalid selection");
+			break; // This break is not really necessary
+		}
+		}
+
+	}
 }
+
