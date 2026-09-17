33ee3b712b098e12dd5f03c913defa0a6b7b4cee
GhostyOG-j <user@192.168.34.171>
2026-09-17 10:38:41 +0200

    Attribute und Methoden zu Pilot hinzugefügt

--- StarSim2099/src/de/oszimt/starsim2099/Pilot.java
+++ StarSim2099/src/de/oszimt/starsim2099/Pilot.java
@@ -9,7 +9,37 @@ package de.oszimt.starsim2099;
 public class Pilot {
 
 	// Attribute
-	
+	String name;
+	double posx;
+	double posy;
+	String grad;
 	// Methoden
-
+	public Pilot() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPosx() {
+		return posx;
+	}
+	public void setPosx(double posx) {
+		this.posx = posx;
+	}
+	public double getPosy() {
+		return posy;
+	}
+	public void setPosy(double posy) {
+		this.posy = posy;
+	}
+	public String getGrad() {
+		return grad;
+	}
+	public void setGrad(String grad) {
+		this.grad = grad;
+	}
+	
 }
