c2a7d27262f055617c5a5f3ad36be1a68c813476
andre <GhostyOG-j@Home.ip>
2026-09-18 13:51:40 +0200

    Die methoden nach den Aufgaben hinzugefuegt

--- Omnom/src/omnom/Haustier.java
+++ Omnom/src/omnom/Haustier.java
@@ -1,61 +1,80 @@
 package omnom;
 
 public class Haustier {
-int hunger;
-int muede;
-int zufrieden;
-int gesund;
-String name;
+	// Attribute
+	int hunger;
+	int muede;
+	int zufrieden;
+	int gesund;
+	String name;
 
-public Haustier(){
-	this.gesund = 100;
-	this.hunger = 100;
-	this.muede = 100;
-	this.zufrieden = 100;
+// Konstruktor
+	public Haustier() {
+		this.gesund = 100;
+		this.hunger = 100;
+		this.muede = 100;
+		this.zufrieden = 100;
+
+	}
+	// Getter und Setter
+
+	public int getHunger() {
+		return hunger;
+	}
+
+	public void setHunger(int hunger) {
+		this.hunger = Math.max(0, Math.min(100, hunger));
+	}
+
+	public int getMuede() {
+		return muede;
+	}
+
+	public void setMuede(int muede) {
+		this.muede = Math.max(0, Math.min(100, hunger));
+	}
+
+	public int getZufrieden() {
+		return zufrieden;
+	}
+
+	public void setZufrieden(int zufrieden) {
+		this.zufrieden = Math.max(0, Math.min(100, hunger));
+	}
+
+	public int getGesund() {
+		return gesund;
+	}
+
+	public void setGesund(int gesund) {
+		this.gesund = Math.max(0, Math.min(100, hunger));
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
-}
-
-public int getHunger() {
-	return hunger;
-}
-
-public void setHunger(int hunger) {
-	this.hunger = Math.max(0, Math.min(100, hunger));
-}
-
-public int getMuede() {
-	return muede;
-}
-
-public void setMuede(int muede) {
-	this.muede = Math.max(0, Math.min(100, hunger));
-}
-
-public int getZufrieden() {
-	return zufrieden;
-}
-
-public void setZufrieden(int zufrieden) {
-	this.zufrieden = Math.max(0, Math.min(100, hunger));
-}
-
-public int getGesund() {
-	return gesund;
-}
-
-public void setGesund(int gesund) {
-	this.gesund = Math.max(0, Math.min(100, hunger));
-}
-
-public String getName() {
-	return name;
-}
-
-public void setName(String name) {
-	this.name = name;
-}
-
-
-
-
+	// Methoden
+	public void fuettern(int wert) {
+		setHunger(this.hunger + wert);
+	}
+	
+	public void schlafen(int wert) {
+		setMuede(this.muede + wert);
+	}
+	
+	public void spielen(int wert) {
+		setZufrieden(this.zufrieden + wert);
+	}
+	
+	public void heilen(int wert) {
+		setGesund(100);
+	}
+	
+	
+	
 }
